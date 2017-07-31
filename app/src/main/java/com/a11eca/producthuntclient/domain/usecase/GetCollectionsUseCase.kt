6195550cdc726d2f1783eabcc549d28d5204e2f4
diklimchuk <klimchuk.daniil@gmail.com>
2017-07-31 17:17:08 +0300

    Implemented logic for collections filtering.

--- app/src/main/java/com/a11eca/producthuntclient/domain/usecase/GetCollectionsUseCase.kt
+++ app/src/main/java/com/a11eca/producthuntclient/domain/usecase/GetCollectionsUseCase.kt
@@ -2,14 +2,23 @@ package com.a11eca.producthuntclient.domain.usecase
 
 import com.a11eca.producthuntclient.domain.entity.ProductCollection
 import com.a11eca.producthuntclient.domain.repo.CollectionsRepo
+import io.reactivex.Completable
 import io.reactivex.Flowable
-import io.reactivex.Single
 import javax.inject.Inject
 
 class GetCollectionsUseCase @Inject constructor(
     private val repo: CollectionsRepo
 ){
-  fun getCollections(categoryId: Long): Flowable<List<ProductCollection>> {
+
+  fun getFilter(): Flowable<Long> {
+    return Flowable.just(0)
+  }
+
+  fun setFilter(categoryId: Long): Completable {
+    return Completable.complete()
+  }
+
+  fun getFiltered(categoryId: Long): Flowable<List<ProductCollection>> {
     return repo.getCollections(categoryId)
   }
 }
--- app/src/main/java/com/a11eca/producthuntclient/presentation/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/a11eca/producthuntclient/presentation/viewmodel/CategoriesViewModel.kt
@@ -6,7 +6,6 @@ import com.a11eca.producthuntclient.domain.usecase.GetCollectionsUseCase
 import com.a11eca.producthuntclient.presentation.entity.CategoriesData
 import com.a11eca.producthuntclient.presentation.livedata.LiveFlow
 import com.a11eca.producthuntclient.presentation.livedata.LiveItems
-import io.reactivex.BackpressureStrategy
 import io.reactivex.android.schedulers.AndroidSchedulers
 import javax.inject.Inject
 
@@ -36,8 +35,18 @@ class CategoriesViewModel @Inject constructor(
         .subscribeWith(LiveFlow<CategoriesData>()))
   }
 
-  fun getCollections(categoryId: Long): LiveFlow<List<ProductCollection>> {
-    return addLocalScopeDisposable(getCollectionsUseCase.getCollections(categoryId)
+  fun setCollectionsFilter(categoryId: Long) {
+    addLocalScopeDisposable(getCollectionsUseCase.setFilter(categoryId)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe({}, {}))
+  }
+
+  fun getCollections(): LiveFlow<List<ProductCollection>> {
+    return addLocalScopeDisposable(getCollectionsUseCase.getFilter()
+        .switchMap {
+          categoryId ->
+          getCollectionsUseCase.getFiltered(categoryId)
+        }
         .observeOn(AndroidSchedulers.mainThread())
         .subscribeWith(LiveFlow<List<ProductCollection>>())
     )
