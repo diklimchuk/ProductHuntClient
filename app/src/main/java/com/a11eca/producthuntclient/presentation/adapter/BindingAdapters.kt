f8729a1bbc99d2fcb0dc937070a77d53bda9bb10
diklimchuk <klimchuk.daniil@gmail.com>
2017-08-01 09:44:53 +0300

    Some work on layouts.

--- app/src/main/java/com/a11eca/producthuntclient/presentation/adapter/BindingAdapters.kt
+++ app/src/main/java/com/a11eca/producthuntclient/presentation/adapter/BindingAdapters.kt
@@ -1,14 +1,19 @@
 package com.a11eca.producthuntclient.presentation.adapter
 
 import android.databinding.BindingAdapter
-import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import com.a11eca.producthuntclient.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget
 
 
-@BindingAdapter("bind:imageUrl", "@bind:error")
-fun loadImage(view: ImageView, url: String?, error: Drawable) {
-  val imageViewTarget = GlideDrawableImageViewTarget(view)
-  Glide.with(view.context).load(url ?: "").error(error).into(imageViewTarget)
+@BindingAdapter("bind:imageUrl")
+fun loadImage(view: ImageView, url: String?) {
+  if (url != null) {
+    val imageViewTarget = GlideDrawableImageViewTarget(view)
+    Glide.with(view.context)
+        .load(url)
+        .error(R.color.colorAccent)
+        .into(imageViewTarget)
+  }
 }
